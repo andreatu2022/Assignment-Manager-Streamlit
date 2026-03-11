348ef81b40869a92fe8ec7e67c9f7620a3db5c1c
Andrea <andreatu@udel.edu>
2026-03-11 12:23:16 -0400

    Updated app_day6.py

--- app_day6.py
+++ app_day6.py
@@ -7,6 +7,19 @@ import time
 
 st.set_page_config(page_title="Course Manager", layout="centered")
 
+#st.session_state remembers the past!
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False #initial value, no one is logged in
+
+if "user" not in st.session_state:
+    st.session_state["user"] = None
+
+if "role" not in st.session_state:
+    st.session_state["role"] = None
+
+if "page" not in st.session_state:
+    st.session_state["page"] = "login"
+
 users = [
     {
         "id": "1",
@@ -18,50 +31,79 @@ users = [
     }
 ]
 
-# --- LOGIN ---
-st.subheader("Log In")
-with st.container(border=True):
-    email_input = st.text_input("Email", key="login_email")
-    password_input = st.text_input("Password", type="password", key="login_password")
-    
-    if st.button("Log In"):
-        with st.spinner("Logging in..."):
-            time.sleep(2) # Fake backend delay
-            
-            # Find user
-            found_user = None
-            for user in users:
-                if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
-                    found_user = user
-                    break
-            
-            if found_user:
-                st.success(f"Welcome back, {found_user['email']}!")
-                time.sleep(2)
-                st.rerun()
-            else:
-                st.error("Invalid credentials")
+if st.session_state["role"] == "instructor":
+    st.markdown("Welcome! This is the Instructor Dashboard")
 
-# --- REGISTRATION ---
-st.subheader("New Instructor Account")
-with st.container(border=True):
-    new_email = st.text_input("Email Address", key="reg_email")
-    new_password = st.text_input("Password", type="password", key="reg_password")
-    
-    if st.button("Create Account"):
-        with st.spinner("Creating account..."):
-            time.sleep(2) # Fake backend delay
-            # ... (Assume validation logic here) ...
-            users.append({
-                "id": str(uuid.uuid4()),
-                "email": new_email,
-                "password": new_password,
-                "role": "Instructor"
-            })
+elif st.session_state["role"] == "Admin":
+    st.markdown("Welcome! This is the Admin Dashboard")
 
-            st.success("Account created!")
+    if st.button("Log Out", type="primary", use_container_width=True):
+        with st.spinner("Logging out..."):
+            st.session_state["logged_in"] = False
+            st.session_state["user"] = None
+            st.session_state["role"] = None
+            st.session_state["page"] = "login"
+            time.sleep(4)
+
+            #After every important action, you need to rerun the page!
             st.rerun()
 
-st.write("---")
-st.dataframe(users)
 
+else:
+    # --- LOGIN ---
+    st.subheader("Log In")
+    with st.container(border=True):
+        email_input = st.text_input("Email", key="login_email")
+        password_input = st.text_input("Password", type="password", key="login_password")
+        
+        if st.button("Log In"):
+            with st.spinner("Logging in..."):
+                time.sleep(2) # Fake backend delay
+                
+                # Find user
+                found_user = None
+                for user in users:
+                    if user["email"].strip().lower() == email_input.strip().lower() and user["password"] == password_input:
+                        found_user = user
+                        break
+                
+                if found_user:
+                    st.success(f"Welcome back, {found_user['email']}!")
+                    st.session_state["logged_in"] = True
+                    st.session_state["user"] = found_user
+                    st.session_state["role"] = found_user["role"]
+
+                    st.sucess("Login sucessful!")
+                    time.sleep(2)
+                    st.rerun()
+                else:
+                    st.error("Invalid credentials")
+
+    # --- REGISTRATION ---
+    st.subheader("New Instructor Account")
+    with st.container(border=True):
+        new_email = st.text_input("Email Address", key="reg_email")
+        new_password = st.text_input("Password", type="password", key="reg_password")
+        
+        if st.button("Create Account"):
+            with st.spinner("Creating account..."):
+                time.sleep(2) # Fake backend delay
+                # ... (Assume validation logic here) ...
+                users.append({
+                    "id": str(uuid.uuid4()),
+                    "email": new_email,
+                    "password": new_password,
+                    "role": "Instructor"
+                })
+
+                st.success("Account created!")
+                st.rerun()
+
+    st.write("---")
+    st.dataframe(users)
+
+with st.sidebar:
+    st.markdown("Course Manager Sidebar")
+    if st.session_state["logged_in"] == True:
+        user = st.session_state["user"]
+        st.markdown(f"Logged User Email: {user["email"]}")
